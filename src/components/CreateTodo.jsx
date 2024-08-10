4617cb756be626bc0e882c2978158de1ff31ebd0
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-10 17:06:41 +0530

    onChange handlers and Addtask function created

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,12 +1,20 @@
 
 
-export function CreateTodo() {
+export function CreateTodo(props) {
+
+    function AddTask(event){
+                
+            
+
+
+    }
 
     return <div>
-        <input type="text" placeholder="title" /> <br />
-        <input type="text" placeholder="description" /> <br />
+        <h1>What's your plan for today??</h1>
+        <input type="text" placeholder="title" /> <br /> <br />
+        <input type="text" placeholder="description" /> <br /> <br />
 
-            <button>Add a todo</button>
+            <button onClick={AddTask}>Add a todo</button>
 
     </div>
 
