5458df5b97636610ac331f1c1922858fd5551035
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-10 18:02:45 +0530

    Header name change to Content-Type

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,20 +1,44 @@
+import { useEffect, useState } from "react"
 
 
 export function CreateTodo(props) {
 
-    function AddTask(event){
-                
-            
+    const [title, setTitle] = useState(" ")
+    const [description, setDescription] = useState(" ")
 
 
+    function AddTask() {
+        console.log(title)
+        console.log(description)
+        fetch("http://localhost:8000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+                title: title,
+                description: description
+
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(
+            async function (res) {
+                const json = await res.json()
+                alert("Todo Added")
+               
+            }
+        )
     }
+
 
     return <div>
         <h1>What's your plan for today??</h1>
-        <input type="text" placeholder="title" /> <br /> <br />
-        <input type="text" placeholder="description" /> <br /> <br />
+        <input type="text" placeholder="title" onChange={(e) => {
 
-            <button onClick={AddTask}>Add a todo</button>
+            setTitle(e.target.value)
+        }} /> <br /> <br />
+        <input type="text" placeholder="description" onChange={(e) => { setDescription(e.target.value) }} /> <br /> <br />
+
+        <button onClick={AddTask}>Add a todo</button>
 
     </div>
 
