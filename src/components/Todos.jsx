bd0708ec66a80c2f972da333aebc8613a5357223
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-11 16:01:04 +0530

    Create updateTask to change the status of todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,18 +1,38 @@
 
 
 export function Todos({ tasks }) {
-console.log(tasks)
+
+    console.log(tasks)
+
+    function updateTask(id) {
+
+        fetch("http://localhost:8000/completed", {
+
+            method: "PUT",
+            body: JSON.stringify({
+                id:id
+
+            }),
+
+            headers:{
+                "Content-Type":"application/json"
+            }
+
+        }) 
+
+
+    }
+
+
+
+    console.log(tasks)
     return <div>
         {tasks.map(function (task) {
             return <div>
                 <h1>{task.title}</h1>
                 <h2>{task.description}</h2>
-                <button> {task.completed === true ? "Completed" : "Mark as Completed"}</button>
+                <button onClick={()=>updateTask(task._id)} id={task._id} > {task.completed === true ? "Completed" : "Mark as Completed"}</button>
             </div>
         })}
     </div>
-
-
-
-
 }
